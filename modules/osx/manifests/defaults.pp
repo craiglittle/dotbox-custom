987155bd232c3541df63ebf5357b592e198ec8ab
Craig Little <craiglttl@gmail.com>
2014-01-29 02:11:36 -0800

    Handle default boolean types more elegantly

--- modules/osx/manifests/defaults.pp
+++ modules/osx/manifests/defaults.pp
@@ -1,10 +1,31 @@
-define osx::defaults ($domain, $key, $value, $user = $id) {
+define osx::defaults ($domain, $key, $value, $type = undef, $user = $id) {
 
   $defaults = '/usr/bin/defaults'
 
+  if $type == undef {
+    $_type = $value ? {
+      true    => 'bool',
+      false   => 'bool',
+      default => 'string'
+    }
+  }
+  else {
+    $_type = $type
+  }
+
+  case $_type {
+    bool: {
+      $_value = $value ? {
+        true  => '1',
+        false => '0'
+      }
+    }
+    default: { $_value = $value }
+  }
+
   exec { "OS X defaults: ${domain}:${key} => ${value}":
-    command => "${defaults} write ${domain} ${key} '${value}'",
-    unless  => "${defaults} read ${domain} ${key} | grep '^${value}$'"
+    command => "${defaults} write ${domain} ${key} -${_type} '${value}'",
+    unless  => "${defaults} read ${domain} ${key} | grep '^${_value}$'"
   }
 
 }
