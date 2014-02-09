f708740b02311f50c2db24482ec01e1c72c9a5b5
Craig Little <craiglttl@gmail.com>
2014-02-09 14:15:11 -0800

    Modify ruby installation settings
    * Increase timeout threshold
    * Stop installing rdoc

--- modules/ruby/manifests/init.pp
+++ modules/ruby/manifests/init.pp
@@ -3,8 +3,8 @@ define ruby ($version = $name) {
   $ruby_dir = "${home}/.rubies"
 
   exec { "Install ruby: ${version}":
-    command => "ruby-install ruby ${version}",
-    require => Package['gcc'],
+    command => "ruby-install -i ${ruby_dir} ruby ${version} -- --disable-install-rdoc",
+    timeout => 600,
     unless  => "ls ${ruby_dir}/ruby-${version}*"
   }
 
